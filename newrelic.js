45b01423bfaaa86a40d968fb7e2bbb1eeb015012
Imanol Yáñez Sastre <lonami80@gmail.com>
2015-04-08 16:21:57 +0200

    Added new relick

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['gemfeed-phantom-server'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'eb6b1bad701765cd8a53ea36ced5d8b0913af9ed',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
